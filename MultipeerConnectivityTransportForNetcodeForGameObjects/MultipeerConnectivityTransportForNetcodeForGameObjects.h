e28545e60375a206fa42350e07b913717fe554b6
yuchen <yuchen@holoi.com>
2022-09-04 20:31:13 +0800

    Initial Commit

---
+++ MultipeerConnectivityTransportForNetcodeForGameObjects/MultipeerConnectivityTransportForNetcodeForGameObjects.h
@@ -0,0 +1,12 @@
+//
+//  MultipeerConnectivityTransportForNetcodeForGameObjects.h
+//  MultipeerConnectivityTransportForNetcodeForGameObjects
+//
+//  Created by Yuchen Zhang on 2022/9/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MultipeerConnectivityTransportForNetcodeForGameObjects : NSObject
+
+@end
---
+++ MultipeerConnectivityTransportForNetcodeForGameObjects/MultipeerConnectivityTransportForNetcodeForGameObjects.m
@@ -0,0 +1,12 @@
+//
+//  MultipeerConnectivityTransportForNetcodeForGameObjects.m
+//  MultipeerConnectivityTransportForNetcodeForGameObjects
+//
+//  Created by Yuchen Zhang on 2022/9/4.
+//
+
+#import "MultipeerConnectivityTransportForNetcodeForGameObjects.h"
+
+@implementation MultipeerConnectivityTransportForNetcodeForGameObjects
+
+@end
